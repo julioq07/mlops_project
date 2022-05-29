f62ba7570afb3a80e9d637e1a23f840aa8be813c
julioq07 <jquinones0709@gmail.com>
2022-05-29 03:52:14 -0500

    collection component added

--- src/bin/main.py
+++ src/bin/main.py
@@ -1,9 +1,26 @@
+import collections
 from log.log import Logger
 
+from lib.collection.collection import LocalCollection
+from conf.collection import modelA  
+
+
+DATASET_NAME = modelA["collection.name"]
+
+
 def main():
-    print("=== Running ===")
+    print("=== Running ML Workflow ===")
+    
     Logger()
 
+    # Data Collection Component
+    print("=== Collection Started ===")
+    colObj = LocalCollection(DATASET_NAME)
+    df = colObj.load_csv()
+    print(df.info)
+    print("=== Collection Ended ===")
+
+    print("=== ML Workflow Ended ===")
 
 if __name__=="__main__":
     main()
